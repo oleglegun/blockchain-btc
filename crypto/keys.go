aba4db33910b54f246da5b8e7f1efadf447ad2a9
Oleg Legun <oleg.legun@gmail.com>
2024-09-10 21:06:15 +0300

    Add Makefile and crypto package
    - Added Makefile with build, run, and test targets
    - Created crypto package with keys.go and keys_test.go files
    - Implemented functions for generating private and public keys, signatures, and addresses
    - Added tests for key generation, signing, and address conversion
    - Added go.mod and go.sum files for dependency management

---
+++ crypto/keys.go
@@ -0,0 +1,119 @@
+package crypto
+
+import (
+	"crypto/ed25519"
+	"crypto/rand"
+	"encoding/hex"
+	"io"
+	"log"
+)
+
+const (
+	privKeyLen = 64
+	pubKeyLen  = 32
+	seedLen    = 32
+	addressLen = 20
+)
+
+func GeneratePrivateKey() PrivateKey {
+	seed := make([]byte, seedLen)
+	_, err := io.ReadFull(rand.Reader, seed)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return PrivateKey{key: ed25519.NewKeyFromSeed(seed)}
+}
+
+func GeneratePrivateKeyFromSeed(seed []byte) PrivateKey {
+	if len(seed) != seedLen {
+		log.Fatalf("Seed length should be %d bytes", seedLen)
+	}
+
+	return PrivateKey{key: ed25519.NewKeyFromSeed(seed)}
+}
+
+func GeneratePrivateKeyFromString(str string) PrivateKey {
+	seed, err := hex.DecodeString(str)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return GeneratePrivateKeyFromSeed(seed)
+}
+
+/*-----------------------------------------------------------------------------
+ *  PrivateKey
+ *----------------------------------------------------------------------------*/
+type PrivateKey struct {
+	key ed25519.PrivateKey
+}
+
+func (p PrivateKey) Bytes() []byte {
+	return p.key
+}
+
+func (p PrivateKey) Sign(msg []byte) Signature {
+	return Signature{
+		value: ed25519.Sign(p.key, msg),
+	}
+}
+
+func (p PrivateKey) Public() PublicKey {
+	b := make([]byte, pubKeyLen)
+	copy(b, p.Bytes()[32:])
+
+	return PublicKey{
+		key: b,
+	}
+}
+
+/*-----------------------------------------------------------------------------
+ *  PublicKey
+ *----------------------------------------------------------------------------*/
+
+type PublicKey struct {
+	key ed25519.PublicKey
+}
+
+func (p PublicKey) Bytes() []byte {
+	return p.key
+}
+
+func (p PublicKey) Address() Address {
+	return Address{
+		// Return the last 20 bytes of the public key
+		value: p.key[len(p.key)-addressLen:],
+	}
+}
+
+/*-----------------------------------------------------------------------------
+ *  Signature
+ *----------------------------------------------------------------------------*/
+
+// Change to pointer if needed (for len and cap)
+type Signature struct {
+	value []byte
+}
+
+func (s Signature) Bytes() []byte {
+	return s.value
+}
+
+func (s Signature) Verify(pubKey PublicKey, msg []byte) bool {
+	return ed25519.Verify(pubKey.key, msg, s.value)
+}
+
+/*-----------------------------------------------------------------------------
+ *  Address
+ *----------------------------------------------------------------------------*/
+
+type Address struct {
+	value []byte
+}
+
+func (a Address) String() string {
+	return hex.EncodeToString(a.value)[0:20]
+}
+
+func (a Address) Bytes() []byte {
+	return a.value
+}
---
+++ crypto/keys_test.go
@@ -0,0 +1,80 @@
+package crypto
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGeneratePrivateKey(t *testing.T) {
+	privKey := GeneratePrivateKey()
+
+	assert.Equal(t, len(privKey.Bytes()), privKeyLen)
+	pubKey := privKey.Public()
+
+	assert.Equal(t, len(pubKey.Bytes()), pubKeyLen)
+}
+
+func TestPrivateKeySign(t *testing.T) {
+	privKey := GeneratePrivateKey()
+	pubKey := privKey.Public()
+
+	differentPrivKey := GeneratePrivateKey()
+	differentPubKey := differentPrivKey.Public()
+
+	msg := []byte("message")
+	differentMsg := []byte("different message")
+	emptyMsg := []byte{}
+
+	t.Run("Valid message verification", func(t *testing.T) {
+		sig := privKey.Sign(msg)
+		if !sig.Verify(pubKey, msg) {
+			t.Errorf("Signature verification failed for valid message")
+		}
+	})
+
+	t.Run("Different message verification", func(t *testing.T) {
+		sig := privKey.Sign(msg)
+		if sig.Verify(pubKey, differentMsg) {
+			t.Errorf("Signature verification should fail for a different message")
+		}
+	})
+
+	t.Run("Empty message verification", func(t *testing.T) {
+		emptySig := privKey.Sign(emptyMsg)
+		if !emptySig.Verify(pubKey, emptyMsg) {
+			t.Errorf("Signature verification failed for empty message")
+		}
+	})
+
+	t.Run("Different keys verification", func(t *testing.T) {
+		sig := privKey.Sign(msg)
+		if sig.Verify(differentPubKey, msg) {
+			t.Errorf("Signature verification should fail for a different public key")
+		}
+	})
+}
+
+func TestNewPrivateKeyFromString(t *testing.T) {
+	var (
+		seed               = "852d9b8e11b181bcf81aad70689521c469a2a5d830a8cbe1df1a497a91c93c84"
+		privKey            = GeneratePrivateKeyFromString(seed)
+		addressStrExpected = "a6461be4eac9ff331cfa"
+		address            = privKey.Public().Address()
+	)
+
+	assert.Equal(t, privKeyLen, len(privKey.Bytes()))
+
+	assert.Equal(t, address.String(), addressStrExpected)
+}
+
+func TestPublicKeyToAddress(t *testing.T) {
+	seed := make([]byte, seedLen)
+
+	privKey := GeneratePrivateKeyFromSeed(seed)
+	pubKey := privKey.Public()
+
+	address := pubKey.Address()
+
+	assert.Equal(t, len(address.Bytes()), addressLen)
+}
