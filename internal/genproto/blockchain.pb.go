9e90d2b4e626a159a9395aef7efcd0ad0109bccc
Oleg Legun <oleg.legun@gmail.com>
2024-09-12 16:30:31 +0300

    Enhance blockchain protocol buffer definitions
    - Add TxInput and TxOutput message types
    - Expand Transaction message with version, inputs, and outputs
    - Update go_package option to reflect correct repository name

--- internal/genproto/blockchain.pb.go
+++ internal/genproto/blockchain.pb.go
@@ -80,11 +80,15 @@ type BlockHeader struct {
 	sizeCache     protoimpl.SizeCache
 	unknownFields protoimpl.UnknownFields
 
-	Version   int32  `protobuf:"varint,1,opt,name=version,proto3" json:"version,omitempty"`
-	Height    int32  `protobuf:"varint,2,opt,name=height,proto3" json:"height,omitempty"` // a sequential id, for 10 blocks - 10
-	PrevHash  []byte `protobuf:"bytes,3,opt,name=prevHash,proto3" json:"prevHash,omitempty"`
-	RootHash  []byte `protobuf:"bytes,4,opt,name=rootHash,proto3" json:"rootHash,omitempty"` // merkle tree root of transactions
-	Timestamp int64  `protobuf:"varint,5,opt,name=timestamp,proto3" json:"timestamp,omitempty"`
+	Version int32 `protobuf:"varint,1,opt,name=version,proto3" json:"version,omitempty"`
+	// Block height in the blockchain (sequential ID)
+	Height int32 `protobuf:"varint,2,opt,name=height,proto3" json:"height,omitempty"`
+	// Hash of the previous block in the chain
+	PrevHash []byte `protobuf:"bytes,3,opt,name=prevHash,proto3" json:"prevHash,omitempty"`
+	// Merkle root hash of all transactions in this block
+	RootHash []byte `protobuf:"bytes,4,opt,name=rootHash,proto3" json:"rootHash,omitempty"`
+	// Unix timestamp when the block was created
+	Timestamp int64 `protobuf:"varint,5,opt,name=timestamp,proto3" json:"timestamp,omitempty"`
 }
 
 func (x *BlockHeader) Reset() {
@@ -154,16 +158,149 @@ func (x *BlockHeader) GetTimestamp() int64 {
 	return 0
 }
 
+type TxInput struct {
+	state         protoimpl.MessageState
+	sizeCache     protoimpl.SizeCache
+	unknownFields protoimpl.UnknownFields
+
+	// PrevTxHash is the hash of the previous transaction that this input is spending from.
+	PrevTxHash []byte `protobuf:"bytes,1,opt,name=prevTxHash,proto3" json:"prevTxHash,omitempty"`
+	// PrevTxOutIndex (UTXO id) is the index of the output in the previous transaction that this input is spending from.
+	PrevTxOutIndex uint32 `protobuf:"varint,2,opt,name=prevTxOutIndex,proto3" json:"prevTxOutIndex,omitempty"`
+	PublicKey      []byte `protobuf:"bytes,3,opt,name=publicKey,proto3" json:"publicKey,omitempty"`
+	Signature      []byte `protobuf:"bytes,4,opt,name=signature,proto3" json:"signature,omitempty"`
+}
+
+func (x *TxInput) Reset() {
+	*x = TxInput{}
+	if protoimpl.UnsafeEnabled {
+		mi := &file_blockchain_proto_msgTypes[2]
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		ms.StoreMessageInfo(mi)
+	}
+}
+
+func (x *TxInput) String() string {
+	return protoimpl.X.MessageStringOf(x)
+}
+
+func (*TxInput) ProtoMessage() {}
+
+func (x *TxInput) ProtoReflect() protoreflect.Message {
+	mi := &file_blockchain_proto_msgTypes[2]
+	if protoimpl.UnsafeEnabled && x != nil {
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		if ms.LoadMessageInfo() == nil {
+			ms.StoreMessageInfo(mi)
+		}
+		return ms
+	}
+	return mi.MessageOf(x)
+}
+
+// Deprecated: Use TxInput.ProtoReflect.Descriptor instead.
+func (*TxInput) Descriptor() ([]byte, []int) {
+	return file_blockchain_proto_rawDescGZIP(), []int{2}
+}
+
+func (x *TxInput) GetPrevTxHash() []byte {
+	if x != nil {
+		return x.PrevTxHash
+	}
+	return nil
+}
+
+func (x *TxInput) GetPrevTxOutIndex() uint32 {
+	if x != nil {
+		return x.PrevTxOutIndex
+	}
+	return 0
+}
+
+func (x *TxInput) GetPublicKey() []byte {
+	if x != nil {
+		return x.PublicKey
+	}
+	return nil
+}
+
+func (x *TxInput) GetSignature() []byte {
+	if x != nil {
+		return x.Signature
+	}
+	return nil
+}
+
+type TxOutput struct {
+	state         protoimpl.MessageState
+	sizeCache     protoimpl.SizeCache
+	unknownFields protoimpl.UnknownFields
+
+	Amount int64 `protobuf:"varint,1,opt,name=amount,proto3" json:"amount,omitempty"`
+	// Address of the recipient
+	Address []byte `protobuf:"bytes,2,opt,name=address,proto3" json:"address,omitempty"`
+}
+
+func (x *TxOutput) Reset() {
+	*x = TxOutput{}
+	if protoimpl.UnsafeEnabled {
+		mi := &file_blockchain_proto_msgTypes[3]
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		ms.StoreMessageInfo(mi)
+	}
+}
+
+func (x *TxOutput) String() string {
+	return protoimpl.X.MessageStringOf(x)
+}
+
+func (*TxOutput) ProtoMessage() {}
+
+func (x *TxOutput) ProtoReflect() protoreflect.Message {
+	mi := &file_blockchain_proto_msgTypes[3]
+	if protoimpl.UnsafeEnabled && x != nil {
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		if ms.LoadMessageInfo() == nil {
+			ms.StoreMessageInfo(mi)
+		}
+		return ms
+	}
+	return mi.MessageOf(x)
+}
+
+// Deprecated: Use TxOutput.ProtoReflect.Descriptor instead.
+func (*TxOutput) Descriptor() ([]byte, []int) {
+	return file_blockchain_proto_rawDescGZIP(), []int{3}
+}
+
+func (x *TxOutput) GetAmount() int64 {
+	if x != nil {
+		return x.Amount
+	}
+	return 0
+}
+
+func (x *TxOutput) GetAddress() []byte {
+	if x != nil {
+		return x.Address
+	}
+	return nil
+}
+
 type Transaction struct {
 	state         protoimpl.MessageState
 	sizeCache     protoimpl.SizeCache
 	unknownFields protoimpl.UnknownFields
+
+	Version int32       `protobuf:"varint,1,opt,name=version,proto3" json:"version,omitempty"`
+	Inputs  []*TxInput  `protobuf:"bytes,2,rep,name=inputs,proto3" json:"inputs,omitempty"`
+	Outputs []*TxOutput `protobuf:"bytes,3,rep,name=outputs,proto3" json:"outputs,omitempty"`
 }
 
 func (x *Transaction) Reset() {
 	*x = Transaction{}
 	if protoimpl.UnsafeEnabled {
-		mi := &file_blockchain_proto_msgTypes[2]
+		mi := &file_blockchain_proto_msgTypes[4]
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 		ms.StoreMessageInfo(mi)
 	}
@@ -176,7 +313,7 @@ func (x *Transaction) String() string {
 func (*Transaction) ProtoMessage() {}
 
 func (x *Transaction) ProtoReflect() protoreflect.Message {
-	mi := &file_blockchain_proto_msgTypes[2]
+	mi := &file_blockchain_proto_msgTypes[4]
 	if protoimpl.UnsafeEnabled && x != nil {
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 		if ms.LoadMessageInfo() == nil {
@@ -189,7 +326,28 @@ func (x *Transaction) ProtoReflect() protoreflect.Message {
 
 // Deprecated: Use Transaction.ProtoReflect.Descriptor instead.
 func (*Transaction) Descriptor() ([]byte, []int) {
-	return file_blockchain_proto_rawDescGZIP(), []int{2}
+	return file_blockchain_proto_rawDescGZIP(), []int{4}
+}
+
+func (x *Transaction) GetVersion() int32 {
+	if x != nil {
+		return x.Version
+	}
+	return 0
+}
+
+func (x *Transaction) GetInputs() []*TxInput {
+	if x != nil {
+		return x.Inputs
+	}
+	return nil
+}
+
+func (x *Transaction) GetOutputs() []*TxOutput {
+	if x != nil {
+		return x.Outputs
+	}
+	return nil
 }
 
 var File_blockchain_proto protoreflect.FileDescriptor
@@ -211,12 +369,30 @@ var file_blockchain_proto_rawDesc = []byte{
 	0x68, 0x12, 0x1a, 0x0a, 0x08, 0x72, 0x6f, 0x6f, 0x74, 0x48, 0x61, 0x73, 0x68, 0x18, 0x04, 0x20,
 	0x01, 0x28, 0x0c, 0x52, 0x08, 0x72, 0x6f, 0x6f, 0x74, 0x48, 0x61, 0x73, 0x68, 0x12, 0x1c, 0x0a,
 	0x09, 0x74, 0x69, 0x6d, 0x65, 0x73, 0x74, 0x61, 0x6d, 0x70, 0x18, 0x05, 0x20, 0x01, 0x28, 0x03,
-	0x52, 0x09, 0x74, 0x69, 0x6d, 0x65, 0x73, 0x74, 0x61, 0x6d, 0x70, 0x22, 0x0d, 0x0a, 0x0b, 0x54,
-	0x72, 0x61, 0x6e, 0x73, 0x61, 0x63, 0x74, 0x69, 0x6f, 0x6e, 0x42, 0x2d, 0x5a, 0x2b, 0x67, 0x69,
-	0x74, 0x68, 0x75, 0x62, 0x2e, 0x63, 0x6f, 0x6d, 0x2f, 0x6f, 0x6c, 0x65, 0x67, 0x6c, 0x65, 0x67,
-	0x75, 0x6e, 0x2f, 0x62, 0x6c, 0x6f, 0x63, 0x6b, 0x63, 0x68, 0x61, 0x69, 0x6e, 0x2d, 0x67, 0x67,
-	0x2f, 0x67, 0x65, 0x6e, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x62, 0x06, 0x70, 0x72, 0x6f, 0x74, 0x6f,
-	0x33,
+	0x52, 0x09, 0x74, 0x69, 0x6d, 0x65, 0x73, 0x74, 0x61, 0x6d, 0x70, 0x22, 0x8d, 0x01, 0x0a, 0x07,
+	0x54, 0x78, 0x49, 0x6e, 0x70, 0x75, 0x74, 0x12, 0x1e, 0x0a, 0x0a, 0x70, 0x72, 0x65, 0x76, 0x54,
+	0x78, 0x48, 0x61, 0x73, 0x68, 0x18, 0x01, 0x20, 0x01, 0x28, 0x0c, 0x52, 0x0a, 0x70, 0x72, 0x65,
+	0x76, 0x54, 0x78, 0x48, 0x61, 0x73, 0x68, 0x12, 0x26, 0x0a, 0x0e, 0x70, 0x72, 0x65, 0x76, 0x54,
+	0x78, 0x4f, 0x75, 0x74, 0x49, 0x6e, 0x64, 0x65, 0x78, 0x18, 0x02, 0x20, 0x01, 0x28, 0x0d, 0x52,
+	0x0e, 0x70, 0x72, 0x65, 0x76, 0x54, 0x78, 0x4f, 0x75, 0x74, 0x49, 0x6e, 0x64, 0x65, 0x78, 0x12,
+	0x1c, 0x0a, 0x09, 0x70, 0x75, 0x62, 0x6c, 0x69, 0x63, 0x4b, 0x65, 0x79, 0x18, 0x03, 0x20, 0x01,
+	0x28, 0x0c, 0x52, 0x09, 0x70, 0x75, 0x62, 0x6c, 0x69, 0x63, 0x4b, 0x65, 0x79, 0x12, 0x1c, 0x0a,
+	0x09, 0x73, 0x69, 0x67, 0x6e, 0x61, 0x74, 0x75, 0x72, 0x65, 0x18, 0x04, 0x20, 0x01, 0x28, 0x0c,
+	0x52, 0x09, 0x73, 0x69, 0x67, 0x6e, 0x61, 0x74, 0x75, 0x72, 0x65, 0x22, 0x3c, 0x0a, 0x08, 0x54,
+	0x78, 0x4f, 0x75, 0x74, 0x70, 0x75, 0x74, 0x12, 0x16, 0x0a, 0x06, 0x61, 0x6d, 0x6f, 0x75, 0x6e,
+	0x74, 0x18, 0x01, 0x20, 0x01, 0x28, 0x03, 0x52, 0x06, 0x61, 0x6d, 0x6f, 0x75, 0x6e, 0x74, 0x12,
+	0x18, 0x0a, 0x07, 0x61, 0x64, 0x64, 0x72, 0x65, 0x73, 0x73, 0x18, 0x02, 0x20, 0x01, 0x28, 0x0c,
+	0x52, 0x07, 0x61, 0x64, 0x64, 0x72, 0x65, 0x73, 0x73, 0x22, 0x6e, 0x0a, 0x0b, 0x54, 0x72, 0x61,
+	0x6e, 0x73, 0x61, 0x63, 0x74, 0x69, 0x6f, 0x6e, 0x12, 0x18, 0x0a, 0x07, 0x76, 0x65, 0x72, 0x73,
+	0x69, 0x6f, 0x6e, 0x18, 0x01, 0x20, 0x01, 0x28, 0x05, 0x52, 0x07, 0x76, 0x65, 0x72, 0x73, 0x69,
+	0x6f, 0x6e, 0x12, 0x20, 0x0a, 0x06, 0x69, 0x6e, 0x70, 0x75, 0x74, 0x73, 0x18, 0x02, 0x20, 0x03,
+	0x28, 0x0b, 0x32, 0x08, 0x2e, 0x54, 0x78, 0x49, 0x6e, 0x70, 0x75, 0x74, 0x52, 0x06, 0x69, 0x6e,
+	0x70, 0x75, 0x74, 0x73, 0x12, 0x23, 0x0a, 0x07, 0x6f, 0x75, 0x74, 0x70, 0x75, 0x74, 0x73, 0x18,
+	0x03, 0x20, 0x03, 0x28, 0x0b, 0x32, 0x09, 0x2e, 0x54, 0x78, 0x4f, 0x75, 0x74, 0x70, 0x75, 0x74,
+	0x52, 0x07, 0x6f, 0x75, 0x74, 0x70, 0x75, 0x74, 0x73, 0x42, 0x2d, 0x5a, 0x2b, 0x67, 0x69, 0x74,
+	0x68, 0x75, 0x62, 0x2e, 0x63, 0x6f, 0x6d, 0x2f, 0x6f, 0x6c, 0x65, 0x67, 0x6c, 0x65, 0x67, 0x75,
+	0x6e, 0x2f, 0x62, 0x6c, 0x6f, 0x63, 0x6b, 0x63, 0x68, 0x61, 0x69, 0x6e, 0x2d, 0x67, 0x67, 0x2f,
+	0x67, 0x65, 0x6e, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x62, 0x06, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x33,
 }
 
 var (
@@ -231,20 +407,24 @@ func file_blockchain_proto_rawDescGZIP() []byte {
 	return file_blockchain_proto_rawDescData
 }
 
-var file_blockchain_proto_msgTypes = make([]protoimpl.MessageInfo, 3)
+var file_blockchain_proto_msgTypes = make([]protoimpl.MessageInfo, 5)
 var file_blockchain_proto_goTypes = []any{
 	(*Block)(nil),       // 0: Block
 	(*BlockHeader)(nil), // 1: BlockHeader
-	(*Transaction)(nil), // 2: Transaction
+	(*TxInput)(nil),     // 2: TxInput
+	(*TxOutput)(nil),    // 3: TxOutput
+	(*Transaction)(nil), // 4: Transaction
 }
 var file_blockchain_proto_depIdxs = []int32{
 	1, // 0: Block.header:type_name -> BlockHeader
-	2, // 1: Block.transactions:type_name -> Transaction
-	2, // [2:2] is the sub-list for method output_type
-	2, // [2:2] is the sub-list for method input_type
-	2, // [2:2] is the sub-list for extension type_name
-	2, // [2:2] is the sub-list for extension extendee
-	0, // [0:2] is the sub-list for field type_name
+	4, // 1: Block.transactions:type_name -> Transaction
+	2, // 2: Transaction.inputs:type_name -> TxInput
+	3, // 3: Transaction.outputs:type_name -> TxOutput
+	4, // [4:4] is the sub-list for method output_type
+	4, // [4:4] is the sub-list for method input_type
+	4, // [4:4] is the sub-list for extension type_name
+	4, // [4:4] is the sub-list for extension extendee
+	0, // [0:4] is the sub-list for field type_name
 }
 
 func init() { file_blockchain_proto_init() }
@@ -278,6 +458,30 @@ func file_blockchain_proto_init() {
 			}
 		}
 		file_blockchain_proto_msgTypes[2].Exporter = func(v any, i int) any {
+			switch v := v.(*TxInput); i {
+			case 0:
+				return &v.state
+			case 1:
+				return &v.sizeCache
+			case 2:
+				return &v.unknownFields
+			default:
+				return nil
+			}
+		}
+		file_blockchain_proto_msgTypes[3].Exporter = func(v any, i int) any {
+			switch v := v.(*TxOutput); i {
+			case 0:
+				return &v.state
+			case 1:
+				return &v.sizeCache
+			case 2:
+				return &v.unknownFields
+			default:
+				return nil
+			}
+		}
+		file_blockchain_proto_msgTypes[4].Exporter = func(v any, i int) any {
 			switch v := v.(*Transaction); i {
 			case 0:
 				return &v.state
@@ -296,7 +500,7 @@ func file_blockchain_proto_init() {
 			GoPackagePath: reflect.TypeOf(x{}).PkgPath(),
 			RawDescriptor: file_blockchain_proto_rawDesc,
 			NumEnums:      0,
-			NumMessages:   3,
+			NumMessages:   5,
 			NumExtensions: 0,
 			NumServices:   0,
 		},
